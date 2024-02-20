b3c56f3e89189adfb1c59b506d449bc66552df26
bangaffannn <bangaffannn@gmail.com>
2024-02-20 22:30:22 +0700

    Testing on Postman

---
+++ backend/models/bookModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const bookSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    publishYear: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+); // Moved timestamps option inside the Schema constructor
+
+export const Book = mongoose.model("Book", bookSchema); // Changed the model name from "Cat" to "Book"
